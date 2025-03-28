d793e7049133ba48f186f6234d07d6a8729aa342
DenysDovgan <dovgan.denys.ws@gmail.com>
2025-03-28 17:51:01 +0100

    replaced vue with nuxt

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+  import {definePageMeta} from "#imports";
+
+  definePageMeta({
+    redirect: '/login',
+  })
+</script>
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,30 +1,10 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { useRouter } from 'vue-router'
-import { useAuthStore } from '@/stores/auth'
-import { Role } from '@/types/models'
-import type { User } from '@/types/models'
-
-import logo from '@/assets/logo.svg'
+import logo from '~/assets/logo.svg'
 
 const email = ref('')
 const password = ref('')
-const router = useRouter()
-const auth = useAuthStore()
 
-const handleLogin = () => {
-  // simulate login
-  const fakeUser: User = {
-    id: 1,
-    email: email.value,
-    role: Role.CLIENT,
-    firstName: 'Test',
-    lastName: 'User',
-  }
-
-  auth.login('fake-jwt-token', fakeUser)
-  router.push('/')
-}
 </script>
 
 <template>
@@ -35,7 +15,7 @@ const handleLogin = () => {
         <img :src="logo" alt="Logo" class="w-50 text-center"/>
       </div>
 
-      <form @submit.prevent="handleLogin">
+      <form @submit.prevent="">
         <div class="mb-4">
           <label class="block text-sm font-medium mb-1">Email</label>
           <input
